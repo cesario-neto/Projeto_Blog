c3eb5c7b101557b779c1f7a23805aabc9449e42e
cesario <cesarioneto7@gmail.com>
2024-02-08 22:33:23 -0300

    register post models in admin

--- app/post/admin.py
+++ app/post/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Post
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = 'title', 'category', 'author'
+    list_filter = 'category__name', 'is_publishe'
+    search_fields = 'title', 'author__username',
