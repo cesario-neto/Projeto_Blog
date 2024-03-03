ff06ff098038293994c87fe4e37b0ad631b16260
cesario-neto <cesarioneto7@gmail.com>
2024-03-03 20:51:28 -0300

    fixing errors

--- app/frontend/urls.py
+++ app/frontend/urls.py
@@ -3,5 +3,5 @@ from .views import PostsListView, PostDetailView
 
 urlpatterns = [
     path('', PostsListView.as_view(), name='home'),
-    path('/post/<slug:slug>', PostDetailView.as_view(), name='post'),
+    path('post/<slug:slug>', PostDetailView.as_view(), name='post'),
 ]
