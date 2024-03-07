a5962ce1780d22301c44c464f7cf9c1d48fed997
cesario-neto <cesarioneto7@gmail.com>
2024-03-06 21:17:49 -0300

    add search view

--- app/frontend/urls.py
+++ app/frontend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostsListView, PostDetailView
+from .views import PostsListView, PostDetailView, SearchPostsListView
 
 urlpatterns = [
     path('', PostsListView.as_view(), name='home'),
     path('post/<slug:slug>', PostDetailView.as_view(), name='post'),
+    path('search/', SearchPostsListView.as_view(), name='search')
 ]
--- app/frontend/views/post_view.py
+++ app/frontend/views/post_view.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.views.generic import ListView, DetailView
 from post.models import Post
 
@@ -15,3 +17,25 @@ class PostDetailView(DetailView):
     context_object_name = 'post'
     template_name = 'post/pages/post.html'
     slug_field = 'slug'
+
+
+class SearchPostsListView(ListView):
+    queryset = Post.objects.filter(is_publishe=True)
+    context_object_name = 'posts'
+    template_name = 'post/pages/home.html'
+    paginate_by = 9
+
+    def get_queryset(self):
+        query = self.request.GET.get('q', '')
+        qs = super().get_queryset()
+
+        if query:
+            qs = qs.filter(title__icontains=query)
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        query = self.request.GET.get('q', '')
+        ctx.update({'query': query})
+        return ctx
